f7e98fc8f57502c922562d7497e9696402645090
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2016-08-26 15:59:31 -0600

    added portfolio

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+var siteInfo = {
+  name: 'Portfolio'
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { sitename: 'Portfolio' });
+  res.render('index', siteInfo);
+});
+
+router.get('/portfolio', function(req, res, next){
+  res.render('portfolio', siteInfo);
 });
 
 module.exports = router;
